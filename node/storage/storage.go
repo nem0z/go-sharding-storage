83bd34e206f4a23a1b5f4659e22bfe76d6740104
nem0z <maxime.bernard.dev@gmail.com>
2023-01-22 21:54:46 +0100

    feat (node/storage): implemente storage using leveldb

--- node/storage/storage.go
+++ node/storage/storage.go
@@ -1,4 +1,36 @@
 package storage
 
+import (
+	"log"
+
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
 type Storage struct {
+	DB *leveldb.DB
+}
+
+func (s *Storage) Init(storage_path string) {
+	db, err := leveldb.OpenFile(storage_path, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.DB = db
+}
+
+func (s *Storage) Close() {
+	s.DB.Close()
+}
+
+func (s *Storage) Get(key []byte) ([]byte, error) {
+	return s.DB.Get(key, nil)
+}
+
+func (s *Storage) Put(key []byte, value []byte) error {
+	return s.DB.Put(key, value, nil)
+}
+
+func (s *Storage) Delete(key []byte) error {
+	return s.DB.Delete(key, nil)
 }
