1708e4a53b80086279c45ea5c480101495b6eaed
nem0z <maxime.bernard.dev@gmail.com>
2023-01-23 12:15:35 +0100

    feat (node/network): feat impelment network struct and init function

--- node/network/network.go
+++ node/network/network.go
@@ -1,4 +1,18 @@
 package network
 
+import (
+	"log"
+	"net/http"
+
+	s "github.com/nem0z/go-sharding-storage/node/storage"
+)
+
 type Network struct {
+	Peers []string
+}
+
+func (n *Network) Init(port string, s *s.Storage) {
+	http.HandleFunc("/file/", HandleFile(s))
+
+	log.Fatal(http.ListenAndServe(port, nil))
 }
