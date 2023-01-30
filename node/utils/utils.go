e74a01f345c913800c9daddf31eb05903de3f926
nem0z <maxime.bernard.dev@gmail.com>
2023-01-30 10:55:07 +0100

    feat (utils): add ExportFile function

--- node/utils/utils.go
+++ node/utils/utils.go
@@ -1,8 +1,28 @@
 package utils
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"os"
+)
+
 func Chunk[T any](items []T, chunkSize int) (chunks [][]T) {
 	for chunkSize < len(items) {
 		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
 	}
 	return append(chunks, items)
+}
+
+func ExportFile(path string, data []byte) error {
+	f, err := os.Create("./files/example_retrive.png")
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
