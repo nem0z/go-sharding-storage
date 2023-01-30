f23a50cad92fa05e487bb3ab39e0aacb3d221b35
nem0z <maxime.bernard.dev@gmail.com>
2023-01-30 11:13:27 +0100

    feat (node): add func start to start handling all request and link with storage

--- node/node/node.go
+++ node/node/node.go
@@ -26,3 +26,29 @@ func (node *Node) Init(storage_path string, http_port string, udp_port string, t
 
 	node.Network = network
 }
+
+func (node *Node) Start() {
+	ch_udp_req := make(chan *n.RequestUDP)
+	ch_udp_resp := make(chan *n.RequestUDP)
+
+	go node.Network.HandleHTTP(node.Storage)
+	go node.Network.HandleUDP(ch_udp_req, ch_udp_resp)
+
+	go func(ch_req chan *n.RequestUDP, ch_resp chan *n.RequestUDP) {
+		for {
+			req := <-ch_req
+			splitted_data := bytes.SplitN(req.Data, []byte(" "), 2)
+			_, hash := splitted_data[0], splitted_data[1]
+
+			file_part, err := node.Storage.Get(hash)
+
+			if err != nil {
+				log.Println("Error retriving file part:", err)
+				continue
+			}
+
+			resp := &n.RequestUDP{Addr: req.Addr, Data: file_part}
+			ch_resp <- resp
+		}
+	}(ch_udp_req, ch_udp_resp)
+}
