c9b63f7141d1edabae4c67f190c30db24933a7d3
nem0z <maxime.bernard.dev@gmail.com>
2023-01-30 11:10:07 +0100

    feat (network): add func broadcast

--- node/network/network.go
+++ node/network/network.go
@@ -22,4 +22,20 @@ func (n *Network) HandleHTTP(s *s.Storage) {
 	err := http.ListenAndServe(n.PortHTTP, nil)
 	log.Fatal(err)
 }
+func (n *Network) Broadcast(message []byte) []byte {
+	ch := make(chan []byte, 1)
+
+	for _, peer := range n.Peers {
+		go func(p *Peer, ch chan []byte) {
+			resp := p.UDP(message)
+
+			if resp != nil {
+				ch <- resp
+			}
+
+		}(peer, ch)
+	}
+
+	resp := <-ch
+	return resp
 }
