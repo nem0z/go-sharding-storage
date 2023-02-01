d25119950b202c86f9075f71f2773d560073aa60
nem0z <maxime.bernard.dev@gmail.com>
2023-02-01 19:48:19 +0100

    feat (node): handle uploaded data from handlers and relay to network

--- node/node/node.go
+++ node/node/node.go
@@ -52,15 +52,15 @@ func (node *Node) Start() {
 			case "get_part":
 				file_part, err := node.Storage.Get(data)
 
-			if err != nil {
+				if err != nil {
 					if err != leveldb.ErrNotFound {
-				log.Println("Error retriving file part:", err)
+						log.Println("Error retriving file part:", err)
 					}
-				continue
-			}
+					continue
+				}
 
-			resp := &n.RequestUDP{Addr: req.Addr, Data: file_part}
-			ch_resp <- resp
+				resp := &n.RequestUDP{Addr: req.Addr, Data: file_part}
+				ch_resp <- resp
 
 			case "relay_part":
 				hash := sha256.Sum256(data)
@@ -68,6 +68,40 @@ func (node *Node) Start() {
 			}
 		}
 	}(ch_udp_req, ch_udp_resp)
+
+	go func(ch chan []byte) {
+		binary_file := <-ch
+
+		chunk_size := 9000
+		chunks := utils.Chunk(binary_file, chunk_size)
+
+		hash_table := make(map[int]string, len(chunks))
+		parts := make([][]byte, len(chunks))
+
+		for i, chunk := range chunks {
+			hash := sha256.Sum256(chunk)
+			hash_table[i] = fmt.Sprintf("%x", hash)
+			parts[i] = chunk
+		}
+
+		file_hash := sha256.Sum256(binary_file)
+		json_hash_table, err := json.Marshal(hash_table)
+
+		if err != nil {
+			log.Println("Error marshaling the hash table")
+			return
+		}
+
+		err = node.Storage.Put(file_hash[:], json_hash_table)
+		if err != nil {
+			log.Println("Error storing the hash table")
+			return
+		}
+
+		node.RelayFile(parts)
+	}(ch_http_parts)
+}
+
 func (node *Node) RelayPart(data []byte) {
 	message := append([]byte("relay_part "), data...)
 	node.Network.Relay(message)
